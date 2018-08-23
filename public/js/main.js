e053acd594e6d60fb4744994296e74ad4eef6373
Sanaei <iabkriman.sanae@gmail.com>
2018-08-23 16:19:05 +0200

    todolist-DONE

--- public/js/main.js
+++ public/js/main.js
@@ -8,93 +8,126 @@ var inputs = document.getElementsByTagName('input')
 var monUl = document.getElementsByTagName('ul')
 
 function newInpt() {
-    var newLi = document.createElement('li')
 
-    newLi.innerHTML = "<span>" + inputs[0].value + "</span>" + "<input class='display-none' value='" + inputs[0].value + "'>" + "<div class='divbtn'><button class='btn1 '> <i class='fas fa-edit'></i></button><button class='btn2'> <i class='fas fa-check-circle'></i></button> <button class='btn3'> <i class='fas fa-trash-alt'></i></button></div>"
-    monUl[0].prepend(newLi)
-    inputs[0].value = ''
-    newLi.classList.add('toDo')
-
-    function edit() {
-        if (event.currentTarget.firstElementChild.classList.contains('fa-edit')) {
-
-            // input perd son display none 
-            newLi.children[0].classList.add('display-none')
-            // input prend la valeur de span
-            newLi.children[1].classList.remove('display-none');
-            event.currentTarget.firstElementChild.classList.remove('fa-edit')
-            event.currentTarget.firstElementChild.classList.add('fa-save')
-            event.currentTarget.classList.add('btn2')
-            event.currentTarget.classList.remove('btn1')
-
-        } else if (event.currentTarget.firstElementChild.classList.contains('fa-save')) {
-            // le span prend la valeur de l'input
-            newLi.children[0].innerHTML = newLi.children[1].value
-            // le span perd son display none
-            newLi.children[0].classList.remove('display-none')
-            // le input prend le display none
-            newLi.children[1].classList.add('display-none')
-            // l'icone change
-            event.currentTarget.firstElementChild.classList.remove('fa-save')
-            event.currentTarget.firstElementChild.classList.add('fa-edit')
-            event.currentTarget.classList.add('btn1')
-            event.currentTarget.classList.remove('btn2')
-        }
-
-    }
-    newLi.children[2].children[0].addEventListener('click', edit);
-
-
-    function toDo() {
-        // si contient une class todo pas de displau none
-        if (newLi.classList.contains('toDo')) {
-            newLi.children[0].classList.remove('display-none')
-        }
-        else{
-            newLi.children[0].classList.add('display-none')
-        }
-
-    }
-    boutons[1].addEventListener('click', toDo)
-
-    function check() {
-        // doit faire passe la couleur de la li en vert quand on clique sur le bouton done
-        newLi.classList.toggle('bg-success')
-        newLi.classList.add('done')
-
-    }
-    newLi.children[2].children[1].addEventListener('click', check);
-
-    function done() {
-        // si continer une class done-> sera en display none ce qui ne content pas 'done'
-        if (newLi.classList.contains('done')) {
-            newLi.children[0].classList.remove('display-none')
-        } else {
-            newLi.classList.add('display-none')
-        }
-    }
-    boutons[3].addEventListener('click', done)
-
-
-
-    function delet() {
-        // doit faire passe la couleur de la li en vert quand on clique sur le bouton done
-        newLi.classList.toggle('bg-danger')
-
-    }
-    newLi.children[2].children[2].addEventListener('click', delet);
-
-    if (newLi.classList.contains('')) {
-        function done() {
-            // doit disparaitre dans todo et dans doing
-
-        }
-
-    } else if (newLi.classList.contains('bg-success')) {
-
-        // quand on clique sur to do: on retrouve tout
-        // quand on clique sur doner: on retrouve les li vertes
-        // quand on clique sur delete:
+    if (inputs[0].value != "") {
+        var newLi = document.createElement('li')
+        newLi.innerHTML = "<span>" + inputs[0].value + "</span>" + "<input class='display-none' value='" + inputs[0].value + "'>" + "<div class='divbtn'><button class='btn1 '> <i class='fas fa-edit'></i></button><button class='btn2'> <i class='fas fa-check-circle'></i></button> <button class='btn3'> <i class='fas fa-trash-alt'></i></button></div>"
+        monUl[0].prepend(newLi)
+        inputs[0].value = ''
+        newLi.classList.add('toDo')
+        newLi.children[2].children[0].addEventListener('click', edit);
+        newLi.children[2].children[1].addEventListener('click', check);
+        newLi.children[2].children[2].addEventListener('click', del);
     }
 }
+
 boutons[0].addEventListener('click', newInpt);
+boutons[1].addEventListener('click', toDo);
+
+function edit() {
+
+    if (event.currentTarget.firstElementChild.classList.contains('fa-edit')) {
+        var newLi = event.currentTarget.parentElement.parentElement;
+        // input perd son display none 
+        newLi.children[0].classList.add('display-none')
+        // input prend la valeur de span
+        newLi.children[1].classList.remove('display-none');
+        event.currentTarget.firstElementChild.classList.remove('fa-edit')
+        event.currentTarget.firstElementChild.classList.add('fa-save')
+        event.currentTarget.classList.add('btn2')
+        event.currentTarget.classList.remove('btn1')
+
+    } else if (event.currentTarget.firstElementChild.classList.contains('fa-save')) {
+        var newLi = event.currentTarget.parentElement.parentElement;
+        // le span prend la valeur de l'input
+        newLi.children[0].innerHTML = newLi.children[1].value
+        // le span perd son display none
+        newLi.children[0].classList.remove('display-none')
+        // le input prend le display none
+        newLi.children[1].classList.add('display-none')
+        // l'icone change
+        event.currentTarget.firstElementChild.classList.remove('fa-save')
+        event.currentTarget.firstElementChild.classList.add('fa-edit')
+        event.currentTarget.classList.add('btn1')
+        event.currentTarget.classList.remove('btn2')
+    }
+
+}
+
+function check() {
+    var newLi = this.parentNode.parentNode
+    newLi.classList.add('bg-success')
+    newLi.classList.add('done')
+    newLi.classList.remove('toDo')
+}
+
+function del() {
+    var newLi = this.parentNode.parentNode
+    // doit faire passe la couleur de la li en vert quand on clique sur le bouton done
+    newLi.classList.add('bg-danger')
+    newLi.classList.add('delete')
+    newLi.classList.remove('toDo')
+    newLi.classList.remove('done')
+}
+
+function toDo() {
+    // si contient une class todo pas de displau none
+    var li = document.getElementsByTagName('li')
+    var tabLi = Array.from(li)
+    tabLi.forEach(element => {
+        if (element.classList.contains('toDo')) {
+            element.classList.remove('display-none')
+            // element.classList.add('display-x')
+        } else {
+            element.classList.add('display-none')
+        }
+    });
+}
+boutons[1].addEventListener('click', toDo);
+
+function done() {
+    var li = document.getElementsByTagName('li')
+    var tabLi = Array.from(li)
+    tabLi.forEach(element => {
+    if (element.classList.contains('done')) {
+        element.classList.remove('display-none')
+
+    } else {
+        element.classList.add('display-none')
+    }
+});
+}
+boutons[3].addEventListener('click', done);
+// doit disparaitre dans todo et dans doing
+// quand on clique sur to do: on retrouve tout
+// quand on clique sur doner: on retrouve les li vertes
+// quand on clique sur delete:
+
+function supprimer() {
+    var li = document.getElementsByTagName('li')
+    var tabLi = Array.from(li)
+    tabLi.forEach(element => {
+    // si continer une class done-> sera en display none ce qui ne content pas 'done'
+    if (element.classList.contains('delete')) {
+        element.classList.remove('display-none')
+
+    } else {
+        element.classList.add('display-none')
+    }
+});
+}
+boutons[2].addEventListener('click', supprimer);
+
+function all() {
+    var li = document.getElementsByTagName('li')
+    var tabLi = Array.from(li)
+    tabLi.forEach(element => {
+    // si continer une class done-> sera en display none ce qui ne content pas 'done'
+    if (element.classList.contains('done'&&'delete')) {
+        element.classList.remove('display-none')
+    } else if (element.classList.contains('toDo')){
+        element.classList.remove('display-none')
+    }
+});
+}
+boutons[4].addEventListener('click', all);
